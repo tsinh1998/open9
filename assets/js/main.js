295f55b188602a03d8624900eed72fd43e391e37
tsinh <tsinh1998@gmail.com>
2023-07-10 20:28:32 +0700

    date-10.07.2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,7 +161,20 @@
                 contentActive.addClass('active').fadeIn("slow");
                 contentActive.siblings().removeClass('active');
                 $(this).addClass('active').parents('.widget-tabs').find('.widget-content-tab').children().eq(liActive).siblings().hide();
-                // swiper_tab();
+            });
+        });
+    };
+
+    var termcondition = function(){
+        $('.flat-tabs').each(function(){
+            $(this).find('.content-tab').children().hide();
+            $(this).find('.content-tab').children(".active").show();
+            $(this).find('.menu-tab').children('li').on('click',function(){
+                var liActive = $(this).index();
+                var contentActive=$(this).siblings().removeClass('active').parents('.flat-tabs').find('.content-tab').children().eq(liActive);
+                contentActive.addClass('active').fadeIn("slow");
+                contentActive.siblings().removeClass('active');
+                $(this).addClass('active').parents('.flat-tabs').find('.content-tab').children().eq(liActive).siblings().hide();
             });
         });
     };
@@ -180,6 +193,111 @@
             $(this).parents(".uploadfile").find(".filename").text(e.target.files[0].name);
           });          
     };
+
+    var flatAccordion = function () {
+        var args = { duration: 600 };
+        $('.flat-toggle .toggle-title.active').siblings('.toggle-content').show();
+        $('.flat-toggle.enable .toggle-title').on('click', function () {
+            $(this).closest('.flat-toggle').find('.toggle-content').slideToggle(args);
+            $(this).toggleClass('active');
+        });
+        $('.flat-accordion .toggle-title').on('click', function () {
+            if (!$(this).is('.active')) {
+                $(this).closest('.flat-accordion').find('.toggle-title.active').toggleClass('active').next().slideToggle(args);
+                $(this).toggleClass('active');
+                $(this).next().slideToggle(args);
+            } else {
+                $(this).toggleClass('active');
+                $(this).next().slideToggle(args);
+            }
+        });
+    };
+
+    var flatAccordion2 = function() {
+        var args = {duration: 600};
+        $('.flat-toggle2 .toggle-title.active').siblings('.toggle-content').show();
+        $('.flat-toggle2.enable .toggle-title').on('click', function() {
+            $(this).closest('.flat-toggle2').find('.toggle-content').slideToggle(args);
+            $(this).toggleClass('active');
+        }); // toggle 
+      
+        $('.flat-accordion2 .toggle-title').on('click', function () {
+            if( !$(this).is('.active') ) {
+                $(this).closest('.flat-accordion2').find('.toggle-title.active').toggleClass('active').next().slideToggle(args);
+                $(this).toggleClass('active');
+                $(this).next().slideToggle(args);
+            } else {
+                $(this).toggleClass('active');
+                $(this).next().slideToggle(args);
+            }     
+        }); // accordion
+
+        // arow
+        $('flat-toggle2').on('click', function(){
+            $(this).addClass('active');
+        });
+    }; 
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var tfChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul'],
+        datasets: [{
+            label: 'Dataset 1',
+            backgroundColor: '#161616',
+            data: [46, 55, 120, 70, 20,50,200],
+            order: 2,         
+        },
+        {
+            label: 'Line Data',
+            data: [36, 35, 100, 50, 20,50,180],
+            borderColor: '#DDF247', 
+            borderWidth: 2, 
+            fill: false, 
+            type: 'line',
+            order: 1,
+            zIndex:2
+        }
+        ]
+        },
+        options: {
+            scales: {
+                x: {
+                grid: {
+                    display: false,
+                }
+                },
+                y: {                   
+                beginAtZero: true,               
+                ticks: {
+                    precision: 0,
+                    stepSize: 50,                    
+                },               
+                }
+            },
+            plugins: {
+                legend: {
+                display: false
+                },
+                tooltip: {
+                    yAlign: 'bottom',
+                    backgroundColor: 'rgba(222, 232, 232, 0.20)',
+                    callbacks: {
+                        label: function (tooltipItem) {
+                            return tooltipItem.value;
+                        }
+                    }
+                },
+                
+            },
+            elements:{
+                bar:{
+                    borderRadius: 20
+                }                
+            }
+        }
+    });
     
 
     // Dom Ready
@@ -190,8 +308,12 @@
         tabs();
         buttonfollow();
         tabs1();
+        termcondition();
         connectwallet();
         flcustominput();
+        flatAccordion();
+        flatAccordion2();
+        tfChart();
     });
 
 })(jQuery);
